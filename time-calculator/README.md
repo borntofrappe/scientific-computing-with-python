d7155d3225f889cc0e0c6fdbdcb482005231b496
borntofrappe <borntofrappe@gmail.com>
2021-12-19 21:49:54 +0100

    complete code assignment

--- time-calculator/README.md
+++ time-calculator/README.md
@@ -2,20 +2,32 @@
 
 ## Preface
 
-[The assignment](https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator) asks to create a function to describe the gap between two dates.
+[The assignment](https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator) asks to create a function to compute a deadline with a start data and a lapse in time.
 
 In terms of **input** the function receives three arguments:
 
-1. a start time in the 12-hour clock format
+1. a start time in the 12-hour clock format. The string includes the AM, PM suffix
 
-2. a duration in hour and minutes
+2. a duration in hour and minutes. Hour can describe any whole number, while minutes is ensured to be a number less than 60
 
-3. an optional starting day of the week
+3. an optional starting day of the week. The string is case insensitive
 
-In terms of **output** the function returns a string adding the duration to the start time. After the end time in the 12-hour clock format the should include additional information following strict rules:
+```py
+add_time("3:00 PM", "3:10")
+add_time("11:43 PM", "24:20", "tueSday")
+```
 
-- the day if passed as third argument
+In terms of **output** the function returns a string adding the lapse to the start date, with the same 12-hour clock format of the input date. Following the hour and minutes the string should include additional information with strict rules:
+
+- the day if passed as third argument, capitalized
 
 - `(next day)` if the date falls on the following day
 
-- `(x days later)` for any following day
+- `(x days later)` for any day exceeding the next one
+
+```text
+6:10 PM
+12:03 AM, Thursday (2 days later)
+```
+
+## Solution
--- time-calculator/script.py
+++ time-calculator/script.py
@@ -1,1 +1,68 @@
-print('Bonne chance')
+def add_time(start, duration, day=""):
+    start_date = start.split()
+    start_time = start_date[0].split(':')
+    start_hour = int(start_time[0])
+    start_minutes = int(start_time[1])
+    start_suffix = start_date[1]
+
+    duration_time = duration.split(':')
+    duration_hour = int(duration_time[0])
+    duration_minutes = int(duration_time[1])
+
+    end_hour = start_hour + duration_hour
+    end_minutes = start_minutes + duration_minutes
+    if end_minutes >= 60:
+        end_hour = end_hour + int(end_minutes / 60)
+        end_minutes = end_minutes % 60
+
+    end_suffix = start_suffix
+    if end_hour >= 12:
+        if int(end_hour / 12) % 2 != 0:
+            end_suffix = end_suffix == 'PM' and 'AM' or 'PM'
+        end_hour = end_hour % 12
+        if end_hour == 0:
+            end_hour = end_hour + 12
+
+    day_excess = int(duration_hour / 24)
+    if start_suffix == 'PM' and end_suffix == 'AM':
+        day_excess = day_excess + 1
+
+    day_suffix = ''
+    if day:
+        start_day = day.lower()
+        days = ['monday', 'tuesday', 'wednesday',
+                'thursday', 'friday', 'saturday', 'sunday']
+        day_index = -1
+        for index in range(len(days)):
+            if days[index] == start_day:
+                day_index = index
+                break
+
+        end_day = days[(day_index + day_excess) % len(days)]
+
+        day_suffix = ', ' + end_day[:1].upper() + end_day[1:]
+
+    excess_suffix = ''
+    if day_excess > 0:
+        if day_excess == 1:
+            excess_suffix = ' (next day)'
+        else:
+            excess_suffix = ' (' + str(day_excess) + ' days later)'
+
+    end = str(end_hour) + ':' + str(end_minutes).rjust(2, '0') + \
+        ' ' + end_suffix + day_suffix + excess_suffix
+    return end
+
+
+print(add_time("3:00 AM", "24:00", 'saturday'))
+print()
+print(add_time("11:30 AM", "2:32", "Monday"))
+print()
+print(add_time("11:43 AM", "00:20"))
+print()
+print(add_time("10:10 PM", "3:30"))
+print()
+print(add_time("11:43 PM", "24:20", "tueSday"))
+print()
+print(add_time("6:30 PM", "205:12"))
+print()
