ba97bbf09f9c9387d92234142fe191992987d825
borntofrappe <borntofrappe@gmail.com>
2021-12-21 10:51:23 +0100

    experiment function

--- probability-calculator/main.py
+++ probability-calculator/main.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 
 class Hat:
@@ -22,5 +23,30 @@ class Hat:
         return balls
 
 
-hat = Hat(red=5, blue=3, green=2)
-print(hat.draw(10))
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    num_experiments_success = 0
+
+    for _ in range(num_experiments):
+        balls = copy.deepcopy(hat).draw(num_balls_drawn)
+
+        has_expected_balls = True
+
+        for color, number in expected_balls.items():
+            if color not in balls or number > balls.count(color):
+                has_expected_balls = False
+                break
+
+        if has_expected_balls:
+            num_experiments_success = num_experiments_success + 1
+
+    return num_experiments_success / num_experiments
+
+
+hat = Hat(blue=4, red=2, green=6)
+probability = experiment(
+    hat=hat,
+    expected_balls={"blue": 2,
+                    "red": 1},
+    num_balls_drawn=4,
+    num_experiments=3000)
+print("Probability:", probability)
